621d18cdea6b217e9799662400dc245777a39b85
Dylan Bayerl <16454634+bayerl@users.noreply.github.com>
2021-03-08 07:59:04 -0500

    Add explicit error catch list to custodian, excluding few nbands err

--- dpgen/generator/lib/cvasp.py
+++ dpgen/generator/lib/cvasp.py
@@ -2,28 +2,39 @@
 # -*- coding: utf-8 -*-
 
 from custodian.vasp.jobs import VaspJob as cvj
-from custodian.vasp.validators import VaspFilesValidator,VasprunXMLValidator
-from custodian.vasp.handlers import VaspErrorHandler,UnconvergedErrorHandler, \
-        NonConvergingErrorHandler,FrozenJobErrorHandler,StdErrHandler,\
-        WalltimeHandler,PositiveEnergyErrorHandler
+from custodian.vasp.validators import VaspFilesValidator, VasprunXMLValidator
+from custodian.vasp.handlers import VaspErrorHandler, UnconvergedErrorHandler, \
+    NonConvergingErrorHandler, FrozenJobErrorHandler, StdErrHandler,\
+    WalltimeHandler, PositiveEnergyErrorHandler
 from custodian import Custodian
 import argparse
 
-handlers=[VaspErrorHandler(),FrozenJobErrorHandler(),StdErrHandler(),NonConvergingErrorHandler(),
-           WalltimeHandler(),PositiveEnergyErrorHandler(),UnconvergedErrorHandler()]
-validators=[VaspFilesValidator(),VasprunXMLValidator()]
+vasperrslist = ['tet', 'inv_rot_mat', 'brmix', 'subspacematrix', 'tetirr',
+                'incorrect_shift', 'real_optlay', 'rspher', 'dentet',
+                'triple_product', 'rot_matrix', 'brions', 'pricel', 'zpotrf',
+                'amin', 'zbrent', 'pssyevx', 'eddrmm', 'edddav',
+                'grad_not_orth', 'nicht_konv', 'zheev', 'elf_kpar', 'elf_ncl',
+                'rhosyg', 'posmap', 'point_group']
 
-def runvasp(cmd,opt=False,max_errors=3,backup=False,auto_gamma=False,auto_npar=False,ediffg=-.05):
+handlers = [VaspErrorHandler(errors_subset_to_catch=vasperrslist), FrozenJobErrorHandler(), StdErrHandler(), NonConvergingErrorHandler(),
+            WalltimeHandler(), PositiveEnergyErrorHandler(), UnconvergedErrorHandler()]
+validators = [VaspFilesValidator(), VasprunXMLValidator()]
+
+
+def runvasp(cmd, opt=False, max_errors=3, backup=False, auto_gamma=False, auto_npar=False, ediffg=-.05):
     """
     cmd example:
     cmd=['mpirun', '-np', '32' , '-machinefile', 'hosts','vasp_std']
     """
     if opt:
-       jobs = cvj.full_opt_run(cmd, auto_npar=auto_npar, ediffg=ediffg, backup=backup, auto_gamma=auto_gamma )
+        jobs = cvj.full_opt_run(
+            cmd, auto_npar=auto_npar, ediffg=ediffg, backup=backup, auto_gamma=auto_gamma)
     else:
-       jobs =  [cvj(cmd, auto_npar=auto_npar, backup=backup, auto_gamma=auto_gamma)]
-    c = Custodian(handlers, jobs, validators=validators,max_errors=max_errors)
+        jobs = [cvj(cmd, auto_npar=auto_npar,
+                    backup=backup, auto_gamma=auto_gamma)]
+    c = Custodian(handlers, jobs, validators=validators, max_errors=max_errors)
     c.run()
+
 
 def __main():
     parser = argparse.ArgumentParser()
@@ -35,11 +46,12 @@ def __main():
     parser.add_argument("MAXERR", type=int,
                         help="The maximum error time for runing vasp")
     args = parser.parse_args()
-    cmd=args.CMD.split()
-    runvasp(cmd=cmd,max_errors=args.MAXERR)
-    
-if __name__=='__main__':
-   __main()
-   #vasp="/sharedext4/vasp/vasp.5.4.4/bin/vasp_std"
-   #runvasp(cmd=['srun', vasp],max_errors=3,backup=True)
-   #runvasp(cmd=['mpirun', '-np', ncpu, fp_cmd],max_errors=max_errors)
+    cmd = args.CMD.split()
+    runvasp(cmd=cmd, max_errors=args.MAXERR)
+
+
+if __name__ == '__main__':
+    __main()
+    # vasp="/sharedext4/vasp/vasp.5.4.4/bin/vasp_std"
+    #runvasp(cmd=['srun', vasp],max_errors=3,backup=True)
+    #runvasp(cmd=['mpirun', '-np', ncpu, fp_cmd],max_errors=max_errors)
